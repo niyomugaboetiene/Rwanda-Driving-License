dccb10844597cd0672676a38e5e58e8f69453ca8
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 13:34:25 +0200

    Final commit of EPSA

---
+++ backend/schema/AdminSchema.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const AdminSchema = new mongoose.Schema({
+    // LicenseExamCategory, ObtainedMarks/20, Decision
+    AdminName: { type: String, required: true },
+    Password: { type: String, required: true },
+});
+
+const Admins = mongoose.model("admins", AdminSchema);
+
+export default AdminSchema;
