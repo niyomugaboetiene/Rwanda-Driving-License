600592860bbdc91a0d6039d9ed8ee290d689b115
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 13:33:18 +0200

    Final commit of EPSA

---
+++ backend/schema/CandidateSchema.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const CandidateSchema = new mongoose.Schema({
+    CandidateNationalId: { type: Number },
+    // CandidateNationalId (PK), FirstName, LastName, Gender, DOB, ExamDate, PhoneNumber (UniqueKey)
+    FirstName: { type: String, required: true },
+    LastName: { type: Number, required: true },
+    Gender: { type: String, required: true },
+    DOB: { type: Date, required: true },
+    ExamDate: { type: Date, required: true },
+    PhoneNumber: { type: Number, required: true }
+});
+
+const Candidate = mongoose.model("candidates", CandidateSchema);
+
+export default Candidate;
--- backend/schema/GradeSchema.js
+++ backend/schema/GradeSchema.js
@@ -1,1 +1,13 @@
 import mongoose from "mongoose";
+
+const GradeSchema = new mongoose.Schema({
+    CandidateNationalId: { type: Number, ref: "candidates", required: true },
+    // LicenseExamCategory, ObtainedMarks/20, Decision
+    LicenseExamCategory: { type: String, required: true },
+    ObtainedMarks: { type: Number, required: true },
+    Decision: { type: String, required: true }
+});
+
+const Grade = mongoose.model("grades", GradeSchema);
+
+export default Grade;
