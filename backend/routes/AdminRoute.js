b6fe366ae8060e66ad44a0910cb2f029c8013af3
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 15:20:56 +0200

    first commit of(RDL)

--- backend/routes/AdminRoute.js
+++ backend/routes/AdminRoute.js
@@ -65,3 +65,20 @@ router.post('/login', async (req, res) => {
         return res.status(500).json({ message: 'Internal server error' });
     }
 });
+
+
+router.post('/logout', async (req, res) => {
+    try {
+         req.session.destroy((err) => {
+            if (err) {
+                console.error(err)
+                return res.status(500).json({ message: 'Failed to logout' });
+            }
+
+            return res.status(200).json({ message: 'Logged out successfully' });
+         })
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
