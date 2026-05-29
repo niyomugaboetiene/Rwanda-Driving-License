754dc2b96adc4caece51111f042c0183a18939d5
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 14:45:22 +0200

    first commit of(RDL)

--- backend/schema/GradeSchema.js
+++ backend/schema/GradeSchema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const GradeSchema = new mongoose.Schema({
     CandidateNationalId: { type: Number, ref: "candidates", required: true },
-    // LicenseExamCategory, ObtainedMarks/20, Decision
+    // CandidateNationalId, LicenseExamCategory, ObtainedMarks/20, Decision
     LicenseExamCategory: { type: String, required: true },
     ObtainedMarks: { type: Number, required: true },
     Decision: { type: String, required: true }
