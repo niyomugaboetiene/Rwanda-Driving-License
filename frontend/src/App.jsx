c8519e5249351be8ef689d8eb4d4851ed893d3ff
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-31 07:59:21 +0200

    first commit of(RDL) frontend based

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import AddCandidate from "./components/candidates/AddCandidate"
 import CandidateList from "./components/candidates/CandidateList"
-import Bro
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
 
   return (
